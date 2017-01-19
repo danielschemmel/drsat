036d1b954a7c8a06007e8c8c3df0440d1c3a21ce
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2017-01-19 21:23:59 +0100

    Update clap config

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,37 @@
+#[macro_use]
 extern crate clap;
 use clap::{App, Arg, SubCommand, AppSettings};
 
-extern crate dsat;
+extern crate libdsat;
 
 include!(concat!(env!("OUT_DIR"), "/version.rs"));
 
 fn main() {
 	let matches = App::new("dsat")
 		.version(VERSION)
-		.subcommand(SubCommand::with_name("dimacs").arg(Arg::with_name("path")
-			.required(true)
-			.index(1)
-			.takes_value(true)
-			.value_name("PATH")
-			.help("The path to the dimacs file")))
-		.subcommand(SubCommand::with_name("stats"))
-    .setting(AppSettings::SubcommandRequiredElseHelp)
-    .get_matches();
+		.about(crate_description!())
+		.setting(AppSettings::ColoredHelp)
+		.setting(AppSettings::GlobalVersion)
+		.setting(AppSettings::SubcommandRequiredElseHelp)
+		.subcommand(SubCommand::with_name("dimacs")
+			.about("Solve a query contained in a dimacs file, as used by the SAT competitions")
+			.arg(Arg::with_name("path")
+				.required(true)
+				.index(1)
+				.takes_value(true)
+				.value_name("PATH")
+				.help("The path to the dimacs file")))
+		.subcommand(SubCommand::with_name("stats").about("Print some internal statistics"))
+		.get_matches();
 
 	match matches.subcommand() {
 		("dimacs", Some(matches)) => {
 			let path = matches.value_of("path").unwrap();
-			dsat::driver::dimacs::run(path);
+			libdsat::driver::dimacs::run(path);
 		}
 		("stats", Some(_)) => {
 			println!("General Purpose AST stats:");
-			dsat::gp::ast::util::print_stats();
+			libdsat::gp::ast::util::print_stats();
 		}
 		_ => {
 			unreachable!();
