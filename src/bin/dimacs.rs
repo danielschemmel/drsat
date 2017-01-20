64091cf382e049f9123326c59cf030681ae60207
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2017-01-20 11:04:37 +0100

    dimacs does not have subcommands

--- src/bin/dimacs.rs
+++ src/bin/dimacs.rs
@@ -10,8 +10,7 @@ fn main() {
 	let matches = libdsat::driver::dimacs::setup_command(App::new("dimacs")
 			.version(VERSION)
 			.setting(AppSettings::ColoredHelp)
-			.setting(AppSettings::GlobalVersion)
-			.setting(AppSettings::SubcommandRequiredElseHelp))
+			.setting(AppSettings::GlobalVersion))
 		.get_matches();
 
 	libdsat::driver::dimacs::main(&matches);
